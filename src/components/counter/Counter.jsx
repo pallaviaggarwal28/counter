2d344f4965a4ec6c2ccd3a5bab0325ee50d318ac
Pallavi Aggarwal <pallaviaggarwal28@gmail.com>
2020-04-20 23:38:52 +0530

    added counter files

---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import "./Counter.css";
+import PropTypes from "prop-types";
+
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    };
+    this.increment = this.increment.bind(this); //No binding required with arrow functions
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+  render() {
+    return (
+      <div className="counter">
+        <CounterButton
+          incrementMethod={this.increment}
+          decrementMethod={this.decrement}
+        ></CounterButton>
+        <CounterButton
+          by={5}
+          incrementMethod={this.increment}
+          decrementMethod={this.decrement}
+        ></CounterButton>
+        <CounterButton
+          by={10}
+          incrementMethod={this.increment}
+          decrementMethod={this.decrement}
+        ></CounterButton>
+        <span className="count">{this.state.counter}</span>
+        <div>
+          <button className="reset" onClick={this.reset}>
+            reset
+          </button>
+        </div>
+      </div>
+    );
+  }
+  increment(by) {
+    this.setState(prevState => {
+      return { counter: prevState.counter + by };
+    });
+  }
+
+  decrement(by) {
+    this.setState(prevState => {
+      return { counter: prevState.counter - by };
+    });
+  }
+
+  reset() {
+    this.setState({
+      counter: 0
+    });
+  }
+}
+class CounterButton extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    };
+  }
+  render() {
+    return (
+      <div className="counter">
+        <button onClick={() => this.props.incrementMethod(this.props.by)}>
+          + {this.props.by}
+        </button>
+        <button onClick={() => this.props.decrementMethod(this.props.by)}>
+          - {this.props.by}
+        </button>
+      </div>
+    );
+  }
+}
+
+CounterButton.defaultProps = {
+  by: 1
+};
+
+CounterButton.propTypes = {
+  by: PropTypes.number
+};
+
+export default Counter;
